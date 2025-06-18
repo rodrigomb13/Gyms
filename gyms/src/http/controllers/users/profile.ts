2024a158865b7bd5c06029bf1bbe8defdb188632
Rodrigo Braz <rodrigomb013@gmail.com>
2025-06-18 15:11:41 -0300

    add outrous req n funcionais e CI

--- gyms/src/http/controllers/users/profile.ts
+++ gyms/src/http/controllers/users/profile.ts
@@ -1,5 +1,5 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
 import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
+import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const getUserProfile = makeGetUserProfileUseCase()
@@ -7,6 +7,7 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const { user } = await getUserProfile.execute({
     userId: request.user.sub,
   })
+
   return reply.status(200).send({
     user: {
       ...user,
